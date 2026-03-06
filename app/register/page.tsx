208f1715aba77103a7b337fb5a286e5c0760ca2d
rhigovillahermosa18-star <rhigovillahermosa18@gmail.com>
2026-03-06 14:02:07 +0800

    update project files

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -4,20 +4,37 @@ import Link from "next/link";
 
 export default function Register() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-[#e8c1cc]">
+    <div
+      className="min-h-screen flex items-center justify-center bg-[#e8c1cc]"
+      style={{
+        backgroundImage: "url('/logo1.png')",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundSize: "500px",
+      }}
+    >
       <div className="bg-white p-8 rounded-xl shadow-md w-80 text-center">
         <h2 className="text-xl text-black font-semibold mb-4">Create Account</h2>
 
-        <input type="text-black" placeholder="Username"
-          className="w-full mb-3 p-2 border rounded text-black" />
+        <input
+          type="text"
+          placeholder="Username"
+          className="w-full mb-3 p-2 border rounded text-black placeholder:text-black"
+        />
 
-        <input type="email text-black" placeholder="Email"
-          className="w-full mb-3 p-2 border rounded text-black" />
+        <input
+          type="email"
+          placeholder="Email"
+          className="w-full mb-3 p-2 border rounded text-black placeholder:text-black"
+        />
 
-        <input type="password text-black" placeholder="Password"
-          className="w-full mb-3 p-2 border rounded text-black" />
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-full mb-3 p-2 border rounded text-black placeholder:text-black"
+        />
 
-        <button className="w-full bg-pink-400 text-white py-2 rounded">
+        <button className="w-full bg-pink-400 text-white py-2 rounded hover:bg-pink-500 transition">
           Sign Up
         </button>
 

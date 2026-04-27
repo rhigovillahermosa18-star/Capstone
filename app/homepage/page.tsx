d5172379862c8f987fe6fe13124502555061e359
rhigovillahermosa18-star <rhigovillahermosa18@gmail.com>
2026-04-27 17:57:04 +0800

    update project files

--- app/homepage/page.tsx
+++ app/homepage/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 
-const nailImages = ["/nail1.jpg","/nail2.jpg","/nail3.jpg","/nail4.jpg","/nail5.jpg","/nail6.jpg","/nail7.jpg","/nail8.jpg","/nail9.jpg","/nail10.jpg","/nail11.jpg"];
+const nailImages = ["/nail1.jpg","/nail2.jpg","/nail3.jpg","/nail4.jpg","/nail5.jpg"];
 
 export default function Home() {
   const router = useRouter();
